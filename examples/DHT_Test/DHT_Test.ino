2b74196fced05273b13ba1e2207f493b3619553f
Stephen Crane <jscrane@gmail.com>
2017-10-05 15:01:41 +0100

    update

--- examples/DHT_Test/DHT_Test.ino
+++ examples/DHT_Test/DHT_Test.ino
@@ -4,9 +4,9 @@ DHT dht;
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
   Serial.println();
-  Serial.println("Status\tHumidity (%)\tTemperature (C)\t(F)");
+  Serial.println("Status\tHumidity (%)\tTemperature (C)");
 
   dht.setup(2); // data pin 2
 }
@@ -18,12 +18,10 @@ void loop()
   float humidity = dht.getHumidity();
   float temperature = dht.getTemperature();
 
-  Serial.print(dht.getStatusString());
+  Serial.print(dht.getStatus());
   Serial.print("\t");
   Serial.print(humidity, 1);
   Serial.print("\t\t");
-  Serial.print(temperature, 1);
-  Serial.print("\t\t");
-  Serial.println(dht.toFahrenheit(temperature), 1);
+  Serial.println(temperature, 1);
 }
 
